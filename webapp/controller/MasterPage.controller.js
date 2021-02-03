b52f8fbf1437fea29189d7b919660fb8332220e8
Dhanush-Mavinakere-Vasanthkumar <dhanushmavinakere@gmail.com>
2021-02-03 13:03:31 +0530

    Initial upload of file

---
+++ webapp/controller/MasterPage.controller.js
@@ -0,0 +1,187 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/GroupHeaderListItem",
+	"sap/ui/Device"
+], function(BaseController, MessageBox, Utilities, History, JSONModel, Filter, FilterOperator, GroupHeaderListItem, Device) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.extraWorkManagement1.controller.MasterPage", {
+		
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("MasterPage").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		},
+		
+		handleRouteMatched: function(oEvent) {
+			
+		},
+		
+		_onStandardListItemPress: function(oEvent) {
+
+			var sID = oEvent.getSource().getSelectedItem().getId();
+			
+			var id = sID.split("-")[sID.split("-").length - 1];
+			
+			this.oRouter.navTo(id);
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onStandardListItemPress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Category", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress2: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Item", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress3: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Requirement", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress4: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Feasibility", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress5: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Billing", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress6: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("ClientApproval", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onStandardListItemPress7: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("BillPosting", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		}
+	});
+}, /* bExport= */ true);
